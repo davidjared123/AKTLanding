d4cb3603ce31376eee264fd265b28494afda5e76
davidjared123 <52287908+davidjared123@users.noreply.github.com>
2020-09-05 12:37:25 -0400

    edicion scroll con js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-$(document).ready(function(){
-    var $cabecera = $('#header');
-    var previousScroll = 0;
-    $(window).scroll(function(event){
-       var scroll = $(this).scrollTop();
-       if (scroll > previousScroll && scroll > 50){
-           $cabecera.addClass('bgcolor');
-       } else {
-           $cabecera.removeClass('bgcolor');
-       }
-       previousScroll = scroll;    });
+// $(document).ready(function(){
+//     var $cabecera = $('#header');
+//     var previousScroll = 0;
+//     $(window).scroll(function(event){
+//        var scroll = $(this).scrollTop();
+//        if (scroll > previousScroll && scroll > 50){
+//            $cabecera.addClass('bgcolor');
+//        } else {
+//            $cabecera.removeClass('bgcolor');
+//        }
+//        previousScroll = scroll;    });
  
-  });
+//   });
+
+
+  $(window).scroll(function() {        
+    var scroll = $(window).scrollTop();    
+    if (scroll >= 100) {
+       $("#header").addClass("bgcolor");    
+    }else{
+       $("#header").removeClass("bgcolor");
+    }
+});
