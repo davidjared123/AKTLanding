60a5a6e45393723731748ec9512c5953451570f2
davidjared123 <52287908+davidjared123@users.noreply.github.com>
2020-09-05 13:35:54 -0400

    save for pull

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-// $(document).ready(function(){
-//     var $cabecera = $('#header');
-//     var previousScroll = 0;
-//     $(window).scroll(function(event){
-//        var scroll = $(this).scrollTop();
-//        if (scroll > previousScroll && scroll > 50){
-//            $cabecera.addClass('bgcolor');
-//        } else {
-//            $cabecera.removeClass('bgcolor');
-//        }
-//        previousScroll = scroll;    });
- 
-//   });
-
-
   $(window).scroll(function() {        
     var scroll = $(window).scrollTop();    
     if (scroll >= 100) {
